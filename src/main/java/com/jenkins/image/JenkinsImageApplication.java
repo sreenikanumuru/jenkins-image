f55ee7bcc33495466d8832c4efec7ce4c3eb071a
sreenivasulukanumuru <skanumuru11>
2022-10-31 19:59:35 +0530

    first commit

---
+++ src/main/java/com/jenkins/image/JenkinsImageApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsImageApplication.class, args);
+	}
+
+}
